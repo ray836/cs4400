8bcff3547f257ca35e2af38fa104a4c28c9269d8
raygrant <raygrant36@gmail.com>
2019-03-22 13:40:42 -0600

    un comment

--- newPip/hellopip.py
+++ newPip/hellopip.py
@@ -182,7 +182,7 @@ class SimpleSwitch13(app_manager.RyuApp):
         data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
             data = msg.data
-        #
-        # out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
-        #                           in_port=in_port, actions=actions, data=data)
-        # datapath.send_msg(out)
+
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions, data=data)
+        datapath.send_msg(out)
