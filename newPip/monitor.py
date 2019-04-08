70a487cc4dcc0ebb37018caccea370bd226f8a56
raygrant <raygrant36@gmail.com>
2019-04-07 22:28:27 -0600

    commented out a lot

--- newPip/monitor.py
+++ newPip/monitor.py
@@ -216,17 +216,16 @@ class Monitor2(app_manager.RyuApp):
             self.backend_reached_count += 1
 
             # matching src(server) to dest(host)
-            print(">>>>>>>>>srcip: ", arp_info.src_ip, " dst_ip: ", arp_info.dst_ip)
-            match = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_IP, ipv4_dst=arp_info.src_ip, ipv4_src=arp_info.dst_ip)
-            actions = [parser.OFPActionOutput(in_port)]
-            self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+            # print(">>>>>>>>>srcip: ", arp_info.src_ip, " dst_ip: ", arp_info.dst_ip)
+            # match = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_IP, ipv4_dst=arp_info.src_ip, ipv4_src=arp_info.dst_ip)
+            # actions = [parser.OFPActionOutput(in_port)]
+            # self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+            #
+            # match = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_IP, ipv4_dst=self.virtual_ip, ipv4_src=arp_info.src_ip)
+            # actions = [parser.OFPActionSetField(ipv4_dst=arp_info.dst_ip), parser.OFPActionOutput(out_port)]
+            # self.add_flow(datapath, 1, match, actions, msg.buffer_id)
 
-            match = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_IP, ipv4_dst=self.virtual_ip, ipv4_src=arp_info.src_ip)
-            actions = [parser.OFPActionSetField(ipv4_dst=arp_info.dst_ip), parser.OFPActionOutput(out_port)]
-            self.add_flow(datapath, 1, match, actions, msg.buffer_id)
 
-            actions = [parser.OFPActionSetField(ipv4_dst=arp_info.dst_ip), parser.OFPActionOutput(out_port)]
-            self.add_flow(datapath, 1, match, actions, buffer_id=ofproto_v1_3.OFP_NO_BUFFER)
 
             # actions = [parser.OFPActionOutput(out_port)]  # parser.OFPActionSetField(ipv4_src="10.0.0.15"),
             # new_match = parser.OFPMatch(in_port=in_port, eth_type=ether_types.ETH_TYPE_IP, ipv4_dst=arp_info.dst_ip)
